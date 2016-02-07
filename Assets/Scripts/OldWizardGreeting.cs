f69d580f3f6052c612f528bd5594394e6579f16c
TobiasMorell <tmorel14@student.aau.dk>
2016-02-07 16:06:17 +0100

    Full push - hoping to fix missing files

---
+++ Assets/Scripts/OldWizardGreeting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OldWizardGreeting : MonoBehaviour {
+    private BoxCollider2D greetingTrigger;
+    private Animator oldWizardAnimator;
+
+    void Start()
+    {
+        greetingTrigger = GetComponent<BoxCollider2D>();
+        oldWizardAnimator = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            oldWizardAnimator.SetTrigger("Greet");
+        }
+    }
+}
