f1d3dc1ec02d25469b7d49ffd585c067ca6e4aea
TobiasMorell <tmorel14@student.aau.dk>
2016-03-16 08:43:50 +0100

    Added paint.net files and states for enemies

---
+++ EnemyStates/IEnemyState.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnemyStates
+{
+	public interface IEnemyState
+	{
+		void Enter(Enemy enemy);
+		void Execute();
+		void Exit();
+		void OnTriggerEnter(UnityEngine.Collider2D other);
+	}
+}
---
+++ EnemyStates/IdleState.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace EnemyStates
+{
+	class IdleState : IEnemyState
+	{
+		public void Enter(Enemy enemy)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Execute()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Exit()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnTriggerEnter(Collider2D other)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
