766535b2b676abfcae9d4686b0eb699abad47d4d
Tobias Morell <tmorel14@student.aau.dk>
2016-03-27 12:14:24 +0200

    Added some behavior to the bat

---
+++ Assets/Scripts/AnimationBehvior/MagicAttackBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MagicAttackBehavior : StateMachineBehaviour {
+	private Wizard wizz;
+
+	public override void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		wizz = animator.GetComponent<Wizard> ();
+		animator.SetBool ("Walking", false);
+		wizz.attacking = true;
+	}
+
+	public override void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		//Spawns a magic bolt
+		var bolt = Instantiate (wizz.Bolt);
+		var boltScrpt = bolt.GetComponent<Bolt> ();
+		if (boltScrpt != null)
+			boltScrpt.movingRight = wizz.facingRight;
+		wizz.attacking = false;
+		bolt.gameObject.transform.position = wizz.BoltSpawnpoint.transform.position;
+	}
+}
---
+++ Assets/Scripts/Enemies/EnemySight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class EnemySight : MonoBehaviour {
+	private Enemy enemy;
+
+	void Start()
+	{
+		this.enemy = GetComponentInParent<Enemy> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			enemy.Target = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+			enemy.Target = null;
+	}
+}
