b6d13f4226cd38ac46907c1c0b786c2c1d8ffc23
TobiasMorell <tmorel14@student.aau.dk>
2016-03-27 17:58:21 +0200

    Added a scene for a simple car game - used as an example of force-based movement

---
+++ Assets/Scripts/MoveCar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCar : MonoBehaviour {
+
+	public float force;
+	private Rigidbody2D rb;
+	public float resistance;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal");
+		if (h != 0f) {
+			rb.AddForce (Vector2.right * h * force);
+			anim.SetBool ("driving", true);
+		} else {
+			rb.AddForce (rb.velocity * resistance);
+			anim.SetBool ("driving", false);
+		}
+	}
+}
