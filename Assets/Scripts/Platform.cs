2025cae107f2e5529803a790b0aed9a2bf7d59ce
Morell <tmorel14@student.aau.dk>
2016-06-30 11:59:01 +0200

    Again added some missing components

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	// Use this for initialization
+	/*void OnTriggerEnter2D(Collider2D other) {
+		string tag = other.tag;
+
+		Debug.Log (other + " entered trigger");
+
+		if (tag.Equals ("Hostile")) {
+			Enemy enem = other.GetComponent<Enemy> ();
+			enem.Flip ();
+		}
+	}*/
+}
---
+++ Assets/Scripts/Settings_click.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Settings_click : MonoBehaviour {
+	bool active = false;
+
+	// Use this for initialization
+	public void Toggle() {
+		this.gameObject.SetActive (!active);
+		active = !active;
+	}
+}
